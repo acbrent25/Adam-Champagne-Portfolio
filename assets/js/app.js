655b0009a7cc4b64972e4a5eaacc2e32970f99e9
Adam Champagne <adamchampagne@Adams-MacBook-Pro.local>
2017-08-31 20:30:02 -0400

    added functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,23 @@ $(document).ready(function() {
     active: 0
     });
 
+    // call owl carousel
+    $(".owl-carousel").owlCarousel();
+
+    $("#owl-demo").owlCarousel({
+             navigation : true, // Show next and prev buttons
+             slideSpeed : 300,
+             paginationSpeed : 400,
+             singleItem:true
+        
+             // "singleItem:true" is a shortcut for:
+             // items : 1, 
+             // itemsDesktop : false,
+             // itemsDesktopSmall : false,
+             // itemsTablet: false,
+             // itemsMobile : false
+        
+         });
+
 
 }); // Document ready
