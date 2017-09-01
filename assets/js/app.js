6fc9ab95e3491e80223bd8f0e14af2d144a142e0
Adam Champagne <adamchampagne@Adams-iMac.fios-router.home>
2017-09-01 12:20:13 -0400

    style and functionality updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,22 +12,26 @@ $(document).ready(function() {
     });
 
     // call owl carousel
-    $(".owl-carousel").owlCarousel();
 
-    $("#owl-demo").owlCarousel({
-             navigation : true, // Show next and prev buttons
-             slideSpeed : 300,
-             paginationSpeed : 400,
-             singleItem:true
-        
-             // "singleItem:true" is a shortcut for:
-             // items : 1, 
-             // itemsDesktop : false,
-             // itemsDesktopSmall : false,
-             // itemsTablet: false,
-             // itemsMobile : false
-        
-         });
+    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        singleItem: true,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 
 
 }); // Document ready
